b2b60f34787f768233fcd389b941f1a7ce9d3bf0
shawnonthemove <1026187367@qq.com>
2023-03-15 21:26:09 +0800

    info: update-vite

--- pro-three-vue/vite.config.js
+++ pro-three-vue/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/projects-for-2023/',
+  base: '/projects-for-2023/pro-three-vue/dist/',
   plugins: [vue()],
   resolve: {
     alias: {
