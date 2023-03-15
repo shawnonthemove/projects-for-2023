15eff9c815a685430a0f643dfef7c1598366b4f0
shawnonthemove <1026187367@qq.com>
2023-03-15 20:07:21 +0800

    info: update-for-vite

--- pro-three-vue/vue3-1/hellovue/vite.config.js
+++ pro-three-vue/vue3-1/hellovue/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/projects-for-2023/pro-three-vue/vue3-1/hellovue/',
   plugins: [vue()],
   resolve: {
     alias: {
