79d4547e4ee829665cdb2c160440eda585e9fe56
shawnonthemove <1026187367@qq.com>
2023-03-16 09:23:18 +0800

    info: add-config

--- pro-three-vue/vite.config.js
+++ pro-three-vue/vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/projects-for-2023/dist/',
   plugins: [vue()],
   resolve: {
     alias: {
